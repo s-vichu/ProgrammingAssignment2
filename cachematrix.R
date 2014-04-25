0be4755686e02fe6560fee7bd4ef4e3a1264faff
Viswanathan S <s_vichu@yahoo.com>
2014-04-25 12:21:01 +0530

    Assignment 2 - First draft

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,64 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix takes a R matrix object and encaptulates it into a function
+## consisting of it's inverse along with getters and setters for both
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    if (is.null(x)) return(NULL)  ## If the argument is NULL, then nothing to do
+    
+    ## Initialize both the original and inverted matrices
+    invert.matrix <- NULL
+    original.matrix <- x
+    
+    ## Set the original matrix using "Super assignment" operator 
+    setMatrix <- function(y) {
+        original.matrix <<- y
+        invert.matrix <<- NULL
+    }
+    
+    ## Get the original matrix
+    getMatrix <- function() {
+        original.matrix
+    }
+    
+    ## Set the inverted matrix
+    setInvert <- function(invert) {
+        invert.matrix <<- invert
+    }
+    
+    ## Get the inverted matrix
+    getInvert <- function() {
+        invert.matrix
+    }
+    
+    ## Return the list with all 4 operator functions
+    list(set = setMatrix, 
+         get = getMatrix, 
+         setInvert = setInvert, 
+         getInvert = getInvert)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve takes a cached matrix object and returns the inverse
+## It checks to see if the invert has already been computed. If so, it confirms that the original
+## matrix has not changed since last invert computation. If not changed, then it returns inverse
+## from the cache. Else it computes the inverse, caches it and then returns
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+    ## Get both the original and inverted matrices
+    invert <- x$getInvert()    
+    
+    ## If the inverse had been computed before, then return from cache
+    if (!is.null(invert)) {
+        ## Returning from the cache
+        message("Returing from cache")
+        return(invert)
+    } 
+    ## Else compute and then return
+    original <- x$get()
+    invert <- solve(original)
+    x$setInvert(invert)
+    invert    
 }
